1d4d4f24d0c9ba24abd6e7ffd750de8284c19959
Chris Jimenez <cjimenez16@gmail.com>
2016-02-02 22:26:17 -0600

    Initial Commit

---
+++ SwiftDelayer/SwiftDelayer.h
@@ -0,0 +1,19 @@
+//
+//  SwiftDelayer.h
+//  SwiftDelayer
+//
+//  Created by Chris Jimenez on 2/2/16.
+//  Copyright © 2016 Chris Jimenez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftDelayer.
+FOUNDATION_EXPORT double SwiftDelayerVersionNumber;
+
+//! Project version string for SwiftDelayer.
+FOUNDATION_EXPORT const unsigned char SwiftDelayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftDelayer/PublicHeader.h>
+
+
